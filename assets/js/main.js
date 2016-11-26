2f57293c7215c7590e8f4fc10ba007b0e095bbbe
Anthony <anthonydevenuto@gmail.com>
2016-11-25 23:34:47 -0600

    reformatted hole sections, started add some functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,27 @@
 $(function(){
 
+  // Open current hole
   $('.hole-info').on('mousedown', function(){
     $(this).parent().toggleClass('open');
   })
+
+  // Change Strokes
+  $('.increase').on('mousedown', function() {
+    var currentNumber = parseInt($(this).next().text());
+    currentNumber += 1;
+    $(this).next().text(currentNumber);
+  });
+  $('.decrease').on('mousedown', function() {
+    var currentNumber = parseInt($(this).prev().text());
+    if (currentNumber>0) {
+      currentNumber -= 1;
+    }
+     $(this).prev().text(currentNumber);
+  });
+
+  // GIR background Color
+  $('.gir-checkbox').on('mousedown', function() {
+    $(this).toggleClass('gir-checked');
+  });
 
 })
