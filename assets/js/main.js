89d5d330ec367daf1987cf8d17429a213301664a
Anthony <anthonydevenuto@gmail.com>
2016-11-28 00:41:47 -0600

    added sass, begining card rebuild

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,27 +1,43 @@
 $(function(){
 
-  // Open current hole
-  $('.hole-info').on('mousedown', function(){
+  // Select 9 or 18 holes
+  $('.nine, .eighteen').on('mousedown', function(e) {
+    $(this).addClass('selected').siblings().removeClass('selected');
+    $('#accordian').empty();
+    $(this).hasClass('nine') ? createCard(9) : createCard(18)
+  });
+  // Generate card
+  function createCard(holes) {
+    for(var i=1;i<holes+1;i++) {
+      $('#accordian').append("<div class='hole'>" +
+                                "<div class='hole-num-container'>" +
+                                    "<p class='hole-num'>Hole:" + i + "</p>" +
+                                "</div>" +
+                             "</div>")
+    }
+  }
+  // OPen current hole
+  $('.hole-num-container').on('mousedown', function(){
     $(this).parent().toggleClass('open');
   })
 
   // Change Strokes
-  $('.increase').on('mousedown', function() {
-    var currentNumber = parseInt($(this).next().text());
-    currentNumber += 1;
-    $(this).next().text(currentNumber);
-  });
-  $('.decrease').on('mousedown', function() {
-    var currentNumber = parseInt($(this).prev().text());
-    if (currentNumber>0) {
-      currentNumber -= 1;
-    }
-     $(this).prev().text(currentNumber);
-  });
+  // $('.increase').on('mousedown', function() {
+  //   var currentNumber = parseInt($(this).next().text());
+  //   currentNumber += 1;
+  //   $(this).next().text(currentNumber);
+  // });
+  // $('.decrease').on('mousedown', function() {
+  //   var currentNumber = parseInt($(this).prev().text());
+  //   if (currentNumber>0) {
+  //     currentNumber -= 1;
+  //   }
+  //    $(this).prev().text(currentNumber);
+  // });
 
   // GIR background Color
-  $('.gir-checkbox').on('mousedown', function() {
-    $(this).toggleClass('gir-checked');
-  });
+  // $('.gir-checkbox').on('mousedown', function() {
+  //   $(this).toggleClass('gir-checked');
+  // });
 
 })
