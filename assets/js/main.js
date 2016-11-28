69edc68de1a431272d28355125b1c9ff1362141f
Anthony <anthonydevenuto@gmail.com>
2016-11-28 14:24:37 -0600

    building new HTML structure to make dynamically adding holes and players easier

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,34 +10,34 @@ $(function(){
   function createCard(holes) {
     for(var i=1;i<holes+1;i++) {
       $('#accordian').append("<div class='hole'>" +
-                                "<div class='hole-num-container'>" +
+                                "<div class='hole-num-container b-bottom'>" +
                                     "<p class='hole-num'>Hole:" + i + "</p>" +
                                 "</div>" +
                              "</div>")
     }
   }
-  // OPen current hole
+  // Open current hole
   $('.hole-num-container').on('mousedown', function(){
     $(this).parent().toggleClass('open');
   })
 
   // Change Strokes
-  // $('.increase').on('mousedown', function() {
-  //   var currentNumber = parseInt($(this).next().text());
-  //   currentNumber += 1;
-  //   $(this).next().text(currentNumber);
-  // });
-  // $('.decrease').on('mousedown', function() {
-  //   var currentNumber = parseInt($(this).prev().text());
-  //   if (currentNumber>0) {
-  //     currentNumber -= 1;
-  //   }
-  //    $(this).prev().text(currentNumber);
-  // });
+  $('.increase').on('mousedown', function() {
+    var currentNumber = parseInt($(this).next().text());
+    currentNumber += 1;
+    $(this).next().text(currentNumber);
+  });
+  $('.decrease').on('mousedown', function() {
+    var currentNumber = parseInt($(this).prev().text());
+    if (currentNumber>0) {
+      currentNumber -= 1;
+    }
+     $(this).prev().text(currentNumber);
+  });
 
   // GIR background Color
-  // $('.gir-checkbox').on('mousedown', function() {
-  //   $(this).toggleClass('gir-checked');
-  // });
+  $('.gir-checkbox').on('mousedown', function() {
+    $(this).toggleClass('gir-checked');
+  });
 
 })
